2849495402aec91dd523a01a563ad554815e32ff
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-17 15:45:06 +0800

    Added get valid password function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,15 +1,21 @@
 PASSWORD = "Pythonista"
 MINIMUM_LENGTH = 0
-def main():
-    password_attempt = input("Enter the Password: ")
 
+def main():
+    password_attempt = get_valid_password()
     while password_attempt != PASSWORD:
-        if password_attempt > MINIMUM_LENGTH:
+        if password_attempt > PASSWORD:
             asterisks = "*" * len(password_attempt)
-            print(f"{asterisks} = Incorrect Password")
-            password_attempt = input("Enter the Password: ")
-        else:
-            print("Invalid Input")
-            password_attempt = input("Enter the Password: ")
+            print(asterisks)
+            print("Incorrect Password")
+            password_attempt = get_valid_password()
+    print("Access Granted")
+
+def get_valid_password():
+    password_attempt = input("Enter the Password: ")
+    while len(password_attempt) == MINIMUM_LENGTH:
+        print("Invalid Input")
+        password_attempt = input("Enter the Password: ")
+    return password_attempt
 
 main()
