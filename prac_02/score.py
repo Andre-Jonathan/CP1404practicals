db03f0fb1378691d66202468b83ea1f5069958cb
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-18 11:08:59 +0800

    Added determine score function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,11 +1,16 @@
 def main():
     score = float(input("Enter score: "))
-    if 0 > score > 100:
-        print("Invalid Input")
-    elif score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    else:
-        print("Bad")
+    score_feedback = determine_score(score)
 
+    print(score_feedback)
+
+def determine_score(score):
+    if 0 > score > 100:
+        score_message = "Invalid Input"
+    elif score >= 90:
+        score_message = "Excellent"
+    elif score >= 50:
+        score_message = "Passable"
+    else:
+        score_message = "Bad"
+    return score_message
