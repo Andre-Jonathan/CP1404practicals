65098c7f59a0c5241d786a3e77f638ee9a68c5de
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-18 11:12:26 +0800

    Added random score and print function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,15 +1,27 @@
+from random import randint
+
+MAXIMUM_SCORE = 100
+HIGH_SCORE = 90
+PASS_SCORE = 50
+MINIMUM_SCORE = 0
+
 def main():
     score = float(input("Enter score: "))
     score_feedback = determine_score(score)
 
     print(score_feedback)
 
+    random_score = randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+    print(random_score)
+    score_feedback = determine_score(random_score)
+    print(score_feedback)
+
 def determine_score(score):
-    if 0 > score > 100:
+    if MINIMUM_SCORE > score > MAXIMUM_SCORE:
         score_message = "Invalid Input"
-    elif score >= 90:
+    elif score >= HIGH_SCORE:
         score_message = "Excellent"
-    elif score >= 50:
+    elif score >= PASS_SCORE:
         score_message = "Passable"
     else:
         score_message = "Bad"
