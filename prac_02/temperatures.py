ce31feb4e5f04dfe833db4fb14dcbe7e65f027a7
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-18 13:29:43 +0800

    Reformatted

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -18,18 +18,18 @@ def main():
     print("Thank you.")
 
 
-
-
 def calculate_celsius():
     """Function to calculate from celsius to fahrenheit"""
     celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
     print(f"Result: {fahrenheit:.2f} °F")
 
+
 def calculate_fahrenheit():
     """Function to calculate from fahrenheit to celsius"""
     fahrenheit = float(input("Fahrenheit: "))
     celsius = 5 / 9 * (fahrenheit - 32)
     print(f"Result: {celsius:.2f} °C")
+
 
 main()
