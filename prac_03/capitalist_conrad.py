c0b9c62cdd0dfc2e35c76d3f3f079b36c52b7d07
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-24 16:06:09 +0800

    Add out_file in the code

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -15,10 +15,12 @@ MIN_PRICE = 1
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 INCREMENT = 1
+OUTPUT_FILE = "capitalist_conrad.txt"
 number_of_days = 0
 
+out_file = open(OUTPUT_FILE, 'w')
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -32,7 +34,8 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
     price *= (1 + price_change)
     number_of_days += INCREMENT
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file = out_file)
+
+out_file.close()
