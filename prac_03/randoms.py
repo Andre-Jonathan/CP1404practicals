81b57bdec052ec23caa5ecb8efe9c0f0e7dd9823
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-24 15:13:46 +0800

    Added comments to answer the questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,30 @@
+"""What did you see on line 1?
+What was the smallest number you could have seen, what was the largest?"""
+
+"""It is a randint function which include the starting 
+number and ending number from 5 to 20."""
+"""The smallest number that could be generated from this 
+code is 5 while the largest number is 20."""
+
+"""What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?"""
+
+"""It is a randrange function which is used to make a sequence 
+from 3 to 10 with increment of 2."""
+"""The smallest number that could be generated is 3 while the 
+largest number is 9."""
+"""Line 2 code will not be able to produce the number 4 because 
+it is a range from 3 to 10 with increment of 2
+which is 3 5 7 9."""
+
+"""What did you see on line 3?
+What was the smallest number you could have seen, what was the largest?"""
+
+"""It is a uniform function which is similar to the randint function 
+except this includes all the decimals between it.
+from 2.5 to 5.5."""
+"""The smallest number that could be generated from this code is 2.5 
+while the largest number is 5.5."""
+
+
