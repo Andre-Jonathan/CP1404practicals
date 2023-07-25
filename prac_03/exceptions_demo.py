6221786f700a045d7a37ab77081ec8a1ce1a607e
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-25 11:43:13 +0800

    Answered the questions

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+ A ValueError will occur when the input is not the data type that is wanted.
+2. When will a ZeroDivisionError occur?
+ A ZeroDivisionError will occur when the calculation is dividing a numerator with zero
+which has an undefined answer.
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+ It is possible by using both "try except" and a while loop which
+keeps on asking for a numerator and denominator value.
+"""
+
