c36ac0a31cf33c7336500600894486130ddb37eb
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-25 12:33:47 +0800

    Add a code where it opens numbers.txt to add 3 numbers into the file

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,11 +1,17 @@
-file = "name.txt"
+NAME_FILE = "name.txt"
 name = input("Enter your name: ")
-out_file = open(file, "w")
+out_file = open(NAME_FILE, "w")
 print(f"Your name is {name}", file=out_file)
 out_file.close()
 
-in_file = open(file, "r")
+in_file = open(NAME_FILE, "r")
 text = in_file.read()
 print(text)
 in_file.close()
 
+NUMBER_FILE = "numbers.txt"
+numbers = [17, 42, 400]
+in_file = open(NUMBER_FILE, "w")
+for number in numbers:
+    print(number, file=in_file)
+in_file.close()
