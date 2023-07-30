711b7b0e64fe2bf893af8b094a4c6ede78772a29
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-30 15:53:53 +0800

    Modify third answer so that it avoid the possibility of a ZeroDivisionError.

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -7,19 +7,22 @@ Answer the following questions:
  A ZeroDivisionError will occur when the calculation is dividing a numerator with zero
 which has an undefined answer.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
- It is possible by using both "try except" and a while loop which
-keeps on asking for a numerator and denominator value.
+It is possible by using a while loop to see if the user input a 0 in the denominator,
+it will ask for another value until it is not a 0 anymore.
 """
 
-while True:
-    try:
-        numerator = int(input("Enter the numerator: "))
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Choose another number.")
         denominator = int(input("Enter the denominator: "))
-        fraction = numerator / denominator
-        print(fraction)
-        False
-    except ValueError:
-        print("Numerator and denominator must be valid numbers!")
-    except ZeroDivisionError:
-        print("Cannot divide by zero!")
+
+    fraction = numerator / denominator
+    print(fraction)
+
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
 print("Finished.")
