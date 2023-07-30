8b67b983ef895331ddb604412e52ec129a90b761
Andre <andre.jonathan@my.jcu.edu.au>
2023-07-30 15:59:14 +0800

    Create a code where it will open the numbers.txt file and take the first 2 numbers and add them together

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,17 +1,24 @@
 NAME_FILE = "name.txt"
-name = input("Enter your name: ")
+name = input("Enter your name: ").title()
 out_file = open(NAME_FILE, "w")
 print(f"Your name is {name}", file=out_file)
 out_file.close()
 
 in_file = open(NAME_FILE, "r")
-text = in_file.read()
+text = in_file.read().strip()
 print(text)
 in_file.close()
 
 NUMBER_FILE = "numbers.txt"
 numbers = [17, 42, 400]
-in_file = open(NUMBER_FILE, "w")
+out_file = open(NUMBER_FILE, "w")
 for number in numbers:
-    print(number, file=in_file)
+    print(number, file=out_file)
+out_file.close()
+
+in_file = open("numbers.txt", "r")
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+print(number1 + number2)
 in_file.close()
+
