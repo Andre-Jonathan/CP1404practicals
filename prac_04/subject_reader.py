4774087248a1a9fc0ac96474349968891c2fd37c
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-01 11:18:03 +0800

    Add code so that it will return the nested list and add a function to print a sentence form of it

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,14 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    info = display_subject(data)
+
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +24,14 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subjects.append(parts)
         print("----------")
     input_file.close()
+    return subjects
 
+def display_subject(subjects):
+    """Display data formatted like: {subject} is taught by {lecturer} and has {number of students} students."""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]}  and has  {subject[2]} students")
 
 main()
