cfbf73fce20b7d94d2ac8325e197d45fe79dbcae
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-03 16:35:28 +0800

    Add modified code to put 6 numbers in list and sort it. Add while loop to prevent repeated numbers in the same line

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,21 @@
 import random
+MINIMUM_LINE = 0
+RANDOM_AMOUNT = 6
+MINIMUM_RANDOM = 1
+MAXIMUM_RANDOM = 45
+numbers = []
 number_of_line = int(input("How many quick picks? "))
+while number_of_line <= MINIMUM_LINE:
+    print("Invalid Input")
+    number_of_line = int(input("How many quick picks? "))
+
 for i in range(number_of_line):
-    for j in range(6):
-        random_number = random.randint(1, 45)
-        print(f"{random_number:3}", end=" ")
-    print()
+    for j in range(RANDOM_AMOUNT):
+        random_number = random.randint(MINIMUM_RANDOM, MAXIMUM_RANDOM)
+        while random_number in numbers:
+            random_number = random.randint(MINIMUM_RANDOM, MAXIMUM_RANDOM)
+        numbers.append(random_number)
+    numbers.sort()
+    print(" ".join(f"{number:3}" for number in numbers))
+    numbers.clear()
+
