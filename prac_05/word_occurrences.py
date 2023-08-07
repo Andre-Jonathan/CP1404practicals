239148b6102408dfec1d2644c369ede2be7b1200
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-07 15:14:34 +0800

    Add modification

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,17 +8,13 @@ count = 0
 words_to_count = {}
 sentence = input("Text: ")
 words = sentence.split()
-print(words)
 
 for word in words:
     count = words_to_count.get(word, 0)
     words_to_count[word] = count + INCREMENT
-print(words_to_count)
 
 words = list(words_to_count.keys())
 words.sort()
-
-print(words)
 
 max_length = max((len(words) for word in words))
 for word in words:
