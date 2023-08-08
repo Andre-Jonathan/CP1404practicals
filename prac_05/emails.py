720969967d07d8efbdb67a37664789cf56b8f76a
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-08 11:26:19 +0800

    Add code add keys and value in the dictionary correctly

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,21 +3,23 @@ Emails
 Estimate: 20 minutes
 Actual:    minutes
 """
-
+FIRST_INDEX = 0
+SECOND_INDEX = 1
 emails = {}
 email = input("Email: ")
 while email != "":
     names = email.split("@")
-    print(names)
-    if "." in names[0]:
-        name = names[0].replace(".", " ")
+    del names[1]
+    if "." in names[FIRST_INDEX]:
+        name = names[FIRST_INDEX].replace(".", " ")
+    else:
+        name = names[FIRST_INDEX]
     name_verification = input(f"Is your name {name.title()}? (Y/n)").title()
     if name_verification == "Y":
-        emails[f"{name.title()}"] = names[1]
-        print(emails)
+        emails[f"{name.title()}"] = email
         email = input("Email: ")
     elif name_verification == "N":
-        name = input("Name: ")
-        emails[f"{name.title()}"] = names[1]
-        print(emails)
+        name = input("Name: ").title()
+        emails[f"{name}"] = email
         email = input("Email: ")
+
