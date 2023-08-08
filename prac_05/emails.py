9448a7c9227d2675efd56bd26c9a428ada6ad9fd
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-08 11:38:56 +0800

    Add minor modification

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,26 +1,32 @@
 """
 Emails
 Estimate: 20 minutes
-Actual:    minutes
+Actual:   40 minutes
 """
 FIRST_INDEX = 0
 SECOND_INDEX = 1
-emails = {}
-email = input("Email: ")
-while email != "":
-    names = email.split("@")
-    del names[1]
-    if "." in names[FIRST_INDEX]:
-        name = names[FIRST_INDEX].replace(".", " ").title()
-    else:
-        name = names[FIRST_INDEX].title()
-    name_verification = input(f"Is your name {name}? (Y/n)").title()
-    if name_verification == "Y":
-        emails[f"{name.title()}"] = email
-        email = input("Email: ")
-    elif name_verification == "N":
-        name = input("Name: ").title()
-        emails[f"{name}"] = email
-        email = input("Email: ")
+def main():
+    emails = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name(email)
+        name_verification = input(f"Is your name {name}? (Y/n)").title()
+        if name_verification == "Y":
+            emails[f"{name}"] = email
+            email = input("Email: ")
+        elif name_verification == "N":
+            name = input("Name: ").title()
+            emails[f"{name}"] = email
+            email = input("Email: ")
 
-print("".join(f"{email} ({emails[email]})\n" for email in emails))
+    print("".join(f"{email} ({emails[email]})\n" for email in emails))
+
+
+def get_name(email):
+    names = email.split("@")
+    del names[SECOND_INDEX]
+    name = names[FIRST_INDEX].replace(".", " ").title()
+    return name
+
+
+main()
