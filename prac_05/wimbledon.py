2489551a9ed094d9d81660a4bc0fb64b53748b9f
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-08 13:19:25 +0800

    Add code to remove unnecesarry information from the file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,10 +1,19 @@
 FILENAME = "wimbledon.csv"
+INCREMENT = 1
+count = 0
 records = []
+libraries = {}
+
 with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
     in_file.readline()
     for row in in_file:
         parts = row.strip().split(",")
         records.append(parts)
 
-        print(row, end="")
-print()
+print(records)
+for i in records:
+    del records[count][0], records[count][2:]
+    count += INCREMENT
+print(records)
+
+
