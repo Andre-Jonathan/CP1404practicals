1c1ccbf1eb2a66fc37c9691ba4f3b9402e84727d
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-10 16:54:04 +0800

    Add slight modification

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,24 +1,23 @@
-"""Time from 1:04 to 2:15
-from 2:20 to 2:50
-from 4:30 to 4:50"""
-
+"""
+Emails
+Estimate: 50 minutes
+Actual:   106 minutes
+"""
 FILENAME = "wimbledon.csv"
 INCREMENT = 1
 count = 0
 records = []
 player_count = {}
+
 with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
     in_file.readline()
     for row in in_file:
         parts = row.strip().split(",")
         records.append(parts)
 
-print(records)
 player_name = [entry[2] for entry in records]
-print(set(player_name))
 
 countries = [country[1] for country in records]
-print(set(countries))
 
 for name in records:
     try:
@@ -26,7 +25,6 @@ for name in records:
     except KeyError:
         player_count[name[2]] = INCREMENT
 
-print(player_count)
 print("Wimbledon Champions:")
 for player in player_count:
     print(f"{player} {player_count[player]}")
