a6e53e484d719c596fc3c3c589626b7277234434
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-10 16:51:25 +0800

    Add code to count different players and print it as intended along with the countries participating

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,9 +1,12 @@
+"""Time from 1:04 to 2:15
+from 2:20 to 2:50
+from 4:30 to 4:50"""
+
 FILENAME = "wimbledon.csv"
 INCREMENT = 1
 count = 0
 records = []
-libraries = {}
-
+player_count = {}
 with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
     in_file.readline()
     for row in in_file:
@@ -11,9 +14,24 @@ with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         records.append(parts)
 
 print(records)
-for i in records:
-    del records[count][0], records[count][2:]
-    count += INCREMENT
-print(records)
+player_name = [entry[2] for entry in records]
+print(set(player_name))
 
+countries = [country[1] for country in records]
+print(set(countries))
 
+for name in records:
+    try:
+        player_count[name[2]] += INCREMENT
+    except KeyError:
+        player_count[name[2]] = INCREMENT
+
+print(player_count)
+print("Wimbledon Champions:")
+for player in player_count:
+    print(f"{player} {player_count[player]}")
+print()
+
+print(f"These {len(set(countries))} countries have won Wimbledon:")
+for country in set(countries):
+    print(country, end=", ")
