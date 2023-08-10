d1710161970f7b6bbb14e18babd4303de6e2d2a8
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-10 16:55:47 +0800

    Add slight modification

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,6 +4,8 @@ Estimate: 50 minutes
 Actual:   106 minutes
 """
 FILENAME = "wimbledon.csv"
+SECOND_INDEX = 1
+THIRD_INDEX = 2
 INCREMENT = 1
 count = 0
 records = []
@@ -15,15 +17,14 @@ with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         parts = row.strip().split(",")
         records.append(parts)
 
-player_name = [entry[2] for entry in records]
 
-countries = [country[1] for country in records]
+countries = [country[SECOND_INDEX] for country in records]
 
 for name in records:
     try:
-        player_count[name[2]] += INCREMENT
+        player_count[name[THIRD_INDEX]] += INCREMENT
     except KeyError:
-        player_count[name[2]] = INCREMENT
+        player_count[name[THIRD_INDEX]] = INCREMENT
 
 print("Wimbledon Champions:")
 for player in player_count:
