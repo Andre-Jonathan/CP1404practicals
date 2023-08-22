f1bd6e74be65ea7e3f206755607d5be6dd414f52
Andre <andre.jonathan@my.jcu.edu.au>
2023-08-23 01:13:14 +0800

    Add get_age and is_vintage function

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,10 +1,20 @@
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
+        """Stores details of a guitar."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Prints statement about the guitar based on the information provided"""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
+    def get_age(self):
+        """Returns the age of the guitar"""
+        CURRENT_YEAR = 2023
+        return CURRENT_YEAR - self.year
 
+    def is_vintage(self):
+        """Checks if guitar is vintage"""
+        VINTAGE_AGE = 50
+        return self.get_age() >= VINTAGE_AGE
