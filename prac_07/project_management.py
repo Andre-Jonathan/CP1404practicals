b08226abb957ecc9a28a709c0c313c5a37f2bba9
Andre <andre.jonathan@my.jcu.edu.au>
2023-09-03 02:11:32 +0800

    Add code to ask for valid date

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -22,22 +22,14 @@ projects = []
 
 
 def main():
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()
-        for row in in_file:
-            part = row.strip().split('\t')
-            name = part[NAME_INDEX]
-            date = part[DATE_INDEX]
-            priority = part[PRIORITY_INDEX]
-            cost_estimate = part[COST_INDEX]
-            completion_estimate = part[COMPLETION_INDEX]
-            project = Project(name, date, priority, cost_estimate, completion_estimate)
-            projects.append(project)
+    extract_file(FILENAME)
     print(projects)
     print(MENU_CHOICE)
     user_pick = input(">>> ").title()
     while user_pick != "Q":
         if user_pick == "L":
+            file_name = input("Enter a filename: ")
+            extract_file(file_name)
             print("Load")
         elif user_pick == "S":
             print("Save")
@@ -50,6 +42,16 @@ def main():
             for project in completed_projects:
                 print(project)
         elif user_pick == "F":
+            date_check = False
+            date = input("Show projects that start after date (dd/mm/yy): ")
+            while date_check:
+                try:
+                    valid_date = datetime.datetime.strptime(date, "%d/%m/%Y").date()
+                    date_check = True
+                except ValueError:
+                    print("Invalid date")
+                    date = input("Show projects that start after date (dd/mm/yy): ")
+
             print("Filter")
         elif user_pick == "A":
             print("Add")
@@ -59,6 +61,20 @@ def main():
             print("Invalid menu choice")
         print(MENU_CHOICE)
         user_pick = input(">>> ").title()
+
+
+def extract_file(filename):
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for row in in_file:
+            part = row.strip().split('\t')
+            name = part[NAME_INDEX]
+            date = part[DATE_INDEX]
+            priority = part[PRIORITY_INDEX]
+            cost_estimate = part[COST_INDEX]
+            completion_estimate = part[COMPLETION_INDEX]
+            project = Project(name, date, priority, cost_estimate, completion_estimate)
+            projects.append(project)
 
 
 def sort_completion(projects):
