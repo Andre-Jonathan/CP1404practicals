1a9ebf727e27538137a4470fccd5092bc343a79d
Andre <andre.jonathan@my.jcu.edu.au>
2023-09-03 01:46:38 +0800

    Add function to divide between the incomplete projects with the complete projects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -17,7 +17,10 @@ DATE_INDEX = 1
 PRIORITY_INDEX = 2
 COST_INDEX = 3
 COMPLETION_INDEX = 4
+COMPLETED_PPERCENTAGE = 100
 projects = []
+
+
 def main():
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
@@ -39,9 +42,12 @@ def main():
         elif user_pick == "S":
             print("Save")
         elif user_pick == "D":
-            sort_priority(projects)
+            incomplete_projects, completed_projects = sort_completion(projects)
             print("Incomplete projects: ")
-            for project in projects:
+            for project in incomplete_projects:
+                print(project)
+            print("Complete projects: ")
+            for project in completed_projects:
                 print(project)
         elif user_pick == "F":
             print("Filter")
@@ -53,5 +59,18 @@ def main():
             print("Invalid menu choice")
         print(MENU_CHOICE)
         user_pick = input(">>> ").title()
+
+
+def sort_completion(projects):
+    """Sort the projects by completion percentage"""
+    incomplete_projects = []
+    completed_projects = []
+    for project in projects:
+        if project.completion_percentage != COMPLETED_PPERCENTAGE:
+            incomplete_projects.append(project)
+        else:
+            completed_projects.append(project)
+    return incomplete_projects, completed_projects
+
 
 main()
