8ea61660cb2e9e0a7cb34e9db6d2e4f4129e9a58
Andre <andre.jonathan@my.jcu.edu.au>
2023-09-03 01:35:05 +0800

    Add code to put informations from projects.txt into a projects list

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,4 +1,6 @@
 from project import Project
+import datetime
+from operator import itemgetter
 
 MENU_CHOICE = """
 (L)oad projects
@@ -9,7 +11,26 @@ MENU_CHOICE = """
 (U)pdate project
 (Q)uit
 """
+FILENAME = "projects.txt"
+NAME_INDEX = 0
+DATE_INDEX = 1
+PRIORITY_INDEX = 2
+COST_INDEX = 3
+COMPLETION_INDEX = 4
+projects = []
 def main():
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for row in in_file:
+            part = row.strip().split('\t')
+            name = part[NAME_INDEX]
+            date = part[DATE_INDEX]
+            priority = part[PRIORITY_INDEX]
+            cost_estimate = part[COST_INDEX]
+            completion_estimate = part[COMPLETION_INDEX]
+            project = Project(name, date, priority, cost_estimate, completion_estimate)
+            projects.append(project)
+    print(projects)
     print(MENU_CHOICE)
     user_pick = input(">>> ").title()
     while user_pick != "Q":
@@ -18,7 +39,10 @@ def main():
         elif user_pick == "S":
             print("Save")
         elif user_pick == "D":
-            print("display")
+            sort_priority(projects)
+            print("Incomplete projects: ")
+            for project in projects:
+                print(project)
         elif user_pick == "F":
             print("Filter")
         elif user_pick == "A":
