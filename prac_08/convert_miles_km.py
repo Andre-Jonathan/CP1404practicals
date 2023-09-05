97afe71348854bdf95982a67a61a0334b2a95f7a
Andre <andre.jonathan@my.jcu.edu.au>
2023-09-05 14:02:29 +0800

    Add slight modification

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,21 +7,24 @@ ERROR_VALUE = 0.0
 
 class MilesConverterApp(App):
     def build(self):
+        """Build Kivy GUI"""
         self.title = "Miles to Kilometer"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_increment(self, change):
+    def handle_increment_or_decrement(self, change):
+        """Increases or decreases the number from the input according to the change"""
         value = self.get_valid_miles() + change
         self.root.ids.input_label.text = str(value)
 
     def handle_conversion(self):
+        """Calculates the conversion from miles to km"""
         value = self.get_valid_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def get_valid_miles(self):
-
+        """Gets a valid value of miles"""
         try:
             value = float(self.root.ids.input_label.text)
         except ValueError:
