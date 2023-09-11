7dd0a40010d4aeb1fa61ecd1a2e832d3a2838a4b
Andre <andre.jonathan@my.jcu.edu.au>
2023-09-11 15:40:38 +0800

    Add Class Variables and modify related codes

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,7 +3,7 @@ from taxi import Taxi
 
 def main():
     """Test taxi class to calculate my_taxi"""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     print(my_taxi)
     my_taxi.drive(40)
     fare = my_taxi.get_fare()
@@ -14,5 +14,6 @@ def main():
     my_taxi.drive(100)
     fare = my_taxi.get_fare()
     print(f"{my_taxi}, and the fair is ${fare} ")
+
 
 main()
