d106702d3544445b2db287f6498e0cffd04a30fc
Andre <andre.jonathan@my.jcu.edu.au>
2023-09-14 12:20:35 +0800

    Add choose taxi code

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,21 +5,36 @@ MENU_OPTION = (""
                "q)uit, "
                "c)hoose taxi, "
                "d)rive")
+MINIMUM_INDEX = 0
+MAXIMUM_INDEX = 3
 
 
 def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    chosen_taxi = ""
+    total_bill = 0
     print("Let's drive!")
     print(MENU_OPTION)
     user_pick = input(">>> ").lower()
     while user_pick != "q":
         if user_pick == "c":
-            print("chose")
+            print("Taxis available:")
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+            user_taxi = int(input("Choose taxi: "))
+            if MAXIMUM_INDEX >= user_taxi > MINIMUM_INDEX:
+                chosen_taxi = taxis[user_taxi]
+                print(chosen_taxi)
+            else:
+                print("Invalid taxi choice")
+            print(f"Bill to date: ${total_bill}")
+
         elif user_pick == "d":
             print("drive")
         else:
             print("Invalid menu choice")
         print(MENU_OPTION)
-        user_pick = input(">>> ").title()
+        user_pick = input(">>> ").lower()
 
 
 main()
